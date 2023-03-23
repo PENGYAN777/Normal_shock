08ffb730aa68793ba6411099fa7430c14b3553d5
PENGYAN777 <peng.yan@mail.polimi.it>
2023-03-23 10:21:04 +0100

    vv

--- vv/coolprop.py
+++ vv/coolprop.py
@@ -40,9 +40,7 @@ print('number of experiment data: ', pe.size)
 fluidname = "MM"
 print("Fluid name:", fluidname)
 R = CP.CoolProp.PropsSI("gas_constant",fluidname)
-# print("universal gas constant:  J/mol/K", R)
 W = CP.CoolProp.PropsSI("molar_mass",fluidname)
-# print("molar mass: kg/mol", W)
 Rs = R/W
 print("spefific ags constant: J/Kg/K", Rs)
 Tc =  CP.CoolProp.PropsSI("Tcrit",fluidname)
@@ -60,11 +58,6 @@ for j in pe.index:
     # print("--------------------Total conditions ------------------")
     pt = pe[j] # total pressure
     tt = te[j]
-    zt = CP.CoolProp.PropsSI('Z','P',pt,'T',tt,fluidname) 
-    # print('Pt[Pa]: ',pt)
-    # print('Tt[K]: ',tt)
-    # print('Zt: ',zt)
-    # dt = CP.CoolProp.PropsSI('Dmass','P',pt,'T',tt,fluidname) 
     s = CP.CoolProp.PropsSI('Smass','P',pt,'T',tt,fluidname) 
     ht = CP.CoolProp.PropsSI('Hmass','P',pt,'T',tt,fluidname) 
     """
@@ -74,26 +67,23 @@ for j in pe.index:
     p = np.linspace(pt*0.1,pt,1000) #
 
     p = pd.Series(p)
-    Z = np.zeros(p.size) # P/rho RT
     h = np.zeros(p.size) # enthalpy
     u = np.zeros(p.size) # velocity
-    v = np.zeros(p.size) # specific volume
     c = np.zeros(p.size) # sound speed
     m = np.zeros(p.size) # Mach number
     d = np.zeros(p.size) # density
     t = np.zeros(p.size) # temperature
-    # g = np.zeros(p.size) # Gamma
 
     for i in p.index:
         if abs(p[i]-Pc)<0.01*Pc:
             p[i] = 0.99*Pc
         d[i] = CP.CoolProp.PropsSI('Dmass','P',p[i],'Smass',s,fluidname) 
         t[i] = CP.CoolProp.PropsSI('T','P',p[i],'Smass',s,fluidname) 
-        Z[i] = CP.CoolProp.PropsSI('Z','P',p[i],'T',t[i],fluidname) 
-        # g[i] = CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','P',p[i],'Smass',s,fluidname) 
-        h[i] = CP.CoolProp.PropsSI('Hmass','T',t[i],'P',p[i],fluidname)
+        h[i] = CP.CoolProp.PropsSI('Hmass','P',p[i],'Smass',s,fluidname) 
+        # h[i] = CP.CoolProp.PropsSI('Hmass','T',t[i],'P',p[i],fluidname)
         u[i] = math.sqrt(abs(2*(ht-h[i])))
-        c[i] = CP.CoolProp.PropsSI('A','P',p[i],'T',t[i],fluidname) 
+        # c[i] = CP.CoolProp.PropsSI('A','P',p[i],'T',t[i],fluidname) 
+        c[i] = CP.CoolProp.PropsSI('A','P',p[i],'Smass',s,fluidname) 
         m[i] = u[i]/c[i]
 
     """
@@ -104,11 +94,11 @@ for j in pe.index:
     # print('M1: ',M1)
     # print("index for required condition:",np.argmin(abs(m-M1)))
     d1 = d[np.argmin(abs(m-M1))]
-    T1 = t[np.argmin(abs(m-M1))]
+    # T1 = t[np.argmin(abs(m-M1))]
     P1 = p[np.argmin(abs(m-M1))]
-    c1 = c[np.argmin(abs(m-M1))]
+    # c1 = c[np.argmin(abs(m-M1))]
     u1 = u[np.argmin(abs(m-M1))]
-    h1 = h[np.argmin(abs(m-M1))]
+    # h1 = h[np.argmin(abs(m-M1))]
     # print('P1[Pa]: ',P1)
     # print('T1[Pa]: ',T1)
 
@@ -117,22 +107,22 @@ for j in pe.index:
     2. input pre-shock conditions
     """
     # d1 = CP.CoolProp.PropsSI('Dmass','P',P1,'T',T1,fluidname) # pre-shock density
-    Z1 = CP.CoolProp.PropsSI('Z','P',P1,'T',T1,fluidname) 
-    G1 = CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','P',P1,'T',T1,fluidname) 
+    # Z1 = CP.CoolProp.PropsSI('Z','P',P1,'T',T1,fluidname) 
+    # G1 = CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','P',P1,'T',T1,fluidname) 
     # print("Z1,Gamma1:", Z1,G1)
-    s1 = CP.CoolProp.PropsSI('Smass','P',P1,'T',T1,fluidname) 
+    # s1 = CP.CoolProp.PropsSI('Smass','P',P1,'T',T1,fluidname) 
     # print("st:", s)
     # print("s1:", s1)
-    cv = CP.CoolProp.PropsSI('Cvmass','P',P1,'T',T1,fluidname) 
-    cp = CP.CoolProp.PropsSI('Cpmass','P',P1,'T',T1,fluidname) 
+    # cv = CP.CoolProp.PropsSI('Cvmass','P',P1,'T',T1,fluidname) 
+    # cp = CP.CoolProp.PropsSI('Cpmass','P',P1,'T',T1,fluidname) 
     # print("gamma:", cp/cv)
-    ht1 = h1 + 0.5*u1*u1
+    # ht1 = h1 + 0.5*u1*u1
     # print("ht:", ht)
     # print("h1:", ht1)
     # compute total pressure/temperature
-    Pt1 = CP.CoolProp.PropsSI('P','Smass',s1,'Hmass',ht1,fluidname) 
-    Tt1 = CP.CoolProp.PropsSI('T','Smass',s1,'Hmass',ht1,fluidname) 
-    htotal1 = CP.CoolProp.PropsSI('Hmass','P',Pt1,'T',Tt1,fluidname) 
+    # Pt1 = CP.CoolProp.PropsSI('P','Smass',s,'Hmass',ht,fluidname) 
+    # Tt1 = CP.CoolProp.PropsSI('T','Smass',s,'Hmass',ht,fluidname) 
+    # htotal1 = CP.CoolProp.PropsSI('Hmass','P',Pt1,'T',Tt1,fluidname) 
     # print("ht1:", htotal1)
 
     """
@@ -151,27 +141,27 @@ for j in pe.index:
         u2[i] = (P1+d1*u1*u1-p2[i])/d1/u1
         if u2[i]>0:
             d2[i] =  d1*u1/u2[i]
-            diff[i] = ht1 - 0.5*u2[i]*u2[i] - CP.CoolProp.PropsSI('Hmass','P',p2[i],'Dmass',d2[i],fluidname) 
+            diff[i] = ht - 0.5*u2[i]*u2[i] - CP.CoolProp.PropsSI('Hmass','P',p2[i],'Dmass',d2[i],fluidname) 
     # print("min index:", np.argmin(abs(diff)))
     P2 = p2[np.argmin(abs(diff))]
     D2 = d2[np.argmin(abs(diff))]    
     T2 = CP.CoolProp.PropsSI('T','P',P2,'Dmass',D2,fluidname) 
-    c2 = CP.CoolProp.PropsSI('A','P',P2,'T',T2,fluidname) 
+    c2 = CP.CoolProp.PropsSI('A','P',P2,'Dmass',D2,fluidname) 
     U2 = u2[np.argmin(abs(diff))]    
     M2 = U2/c2
-    h2 = CP.CoolProp.PropsSI('Hmass','P',P2,'Dmass',D2,fluidname) 
-    htotal2 = h2 + 0.5*U2*U2
+    h2 = CP.CoolProp.PropsSI('Hmass','P',P2,'Dmass',D2,fluidname)
+    ht2 = h2 + 0.5*U2*U2
     # print("ht2:", htotal2)
     # print("(ht2-ht1)/ht1(%):", (htotal2-htotal1)/htotal1*100)
     # print("M2:", M2)
     # print("P2/P1:", P2/P1)
     # print("T2/T1:", T2/T1)
     # print("D2/D1:", D2/d1)
-    s2 = CP.CoolProp.PropsSI('Smass','P',P2,'T',T2,fluidname) 
-    Pt2 = CP.CoolProp.PropsSI('P','Smass',s2,'Hmass',ht1,fluidname) 
-    Tt2 = CP.CoolProp.PropsSI('T','Smass',s2,'Hmass',ht1,fluidname) 
-    Y[j] = (Pt1-Pt2)/(0.5*u1*u1)*100
-    Diff[j] = (htotal2-htotal1)/htotal1*100
+    s2 = CP.CoolProp.PropsSI('Smass','P',P2,'Dmass',D2,fluidname) 
+    Pt2 = CP.CoolProp.PropsSI('P','Smass',s2,'Hmass',ht2,fluidname) 
+    # Tt2 = CP.CoolProp.PropsSI('T','Smass',s2,'Hmass',ht2,fluidname) 
+    Y[j] = (pt-Pt2)/(pt-P1)*100
+    Diff[j] = (ht2-ht)/ht*100
 
 """
 4. write into csv file
--- vv/plot.py
+++ vv/plot.py
@@ -12,42 +12,51 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 data= pd.read_csv("result.csv", ",", skiprows=0)
+Yc= pd.read_csv("Yc.csv", ",", header=None)
 
 
 # n = 10
 # colors = plt.cm.tab20(np.linspace(0, 1, n))
-fig1 = plt.figure( dpi=300)
+fig1, ax = plt.subplots(3)
+fig1.suptitle('Comparison between theoretical solutions and previous work')
+# fig1 = plt.figure( dpi=300)
 lwh = 2
-axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,-2]*1.05 , 'k', lw=lwh, label="$\pm$ 5% bar")
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,-2]*0.95 , 'k', lw=lwh)
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,-2] , 'ko', lw=lwh, label="Experiment data")
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,-1] , 'ro', lw=lwh, label="theoretical solutions")
+# ax[0] = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax[0].plot(data.iloc[:,2]/1e5 , data.iloc[:,-2] , 'ko', lw=lwh, label="Experiment data (Conti et.al 2022)")
+ax[0].plot(data.iloc[:,2]/1e5 , Yc.iloc[:,-1] , 'bo', lw=lwh, label="Calculated results (Conti et.al 2022)")
+ax[0].plot(data.iloc[:,2]/1e5 , data.iloc[:,-1] , 'ro', lw=lwh, label="Theoretical solutions")
 
 
-axes.set_xlabel('$P_t$ $[bar]$',fontsize=12)
-axes.set_ylabel('Y(%)',fontsize=12) 
+ax[0].set_xlabel('$P_t$ $[bar]$',fontsize=12)
+ax[0].set_ylabel('Y(%)',fontsize=12) 
 # axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
-axes.set_ylim([10, 20])
-axes.legend(loc=0 , prop={'size': 10}) # 
+ax[0].set_ylim([15, 25])
+ax[0].legend(loc=0 , prop={'size': 7}) # 
+
+ax[1].plot(data.iloc[:,2]/1e5 , data.iloc[:,4] , 'ko', lw=lwh)
+ax[1].set_xlabel('$P_t$ $[bar]$',fontsize=12)
+ax[1].set_ylabel('$M_1$',fontsize=12) 
+# axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
+ax[1].set_ylim([1.4, 1.6])
+
+# ax[2] = fig1.add_axes([0.15, 0.15, 0.2, 0.2]) #size of figure
+ax[2].plot(data.iloc[:,2]/1e5 , data.iloc[:,3]-273.15 , 'ko')
+ax[2].set_xlabel('$P_t$ $[bar]$',fontsize=12)
+ax[2].set_ylabel('$T_t$ [K]',fontsize=12) 
+# axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
+ax[2].set_ylim([200, 240])
+# ax[1].legend(loc=0 , prop={'size': 10}) # 
+
 fig1.savefig("vv.eps")
 
-fig2 = plt.figure( dpi=300)
-lwh = 2
-axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,3]-273.15 , 'ko', lw=lwh, label="Experiment data")
-axes.set_xlabel('$P_t$ $[bar]$',fontsize=12)
-axes.set_ylabel('$T_t$ [K]',fontsize=12) 
-# axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
-axes.set_ylim([160, 240])
-axes.legend(loc=0 , prop={'size': 10}) # 
 
-fig3 = plt.figure( dpi=300)
-lwh = 2
-axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
-axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,4] , 'ko', lw=lwh, label="Experiment data")
-axes.set_xlabel('$P_t$ $[bar]$',fontsize=12)
-axes.set_ylabel('Mach',fontsize=12) 
-# axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
-axes.set_ylim([1.35, 1.65])
-axes.legend(loc=0 , prop={'size': 10}) # 
+
+# fig3 = plt.figure( dpi=300)
+# lwh = 2
+# axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+# axes.plot(data.iloc[:,2]/1e5 , data.iloc[:,4] , 'ko', lw=lwh, label="Experiment data")
+# axes.set_xlabel('$P_t$ $[bar]$',fontsize=12)
+# axes.set_ylabel('$M_1$',fontsize=12) 
+# # axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
+# axes.set_ylim([1.35, 1.65])
+# axes.legend(loc=0 , prop={'size': 10}) # 
