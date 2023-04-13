d3e143b2a93e77edf7a1a281b7be67ae930e2509
PENGYAN777 <peng.yan@mail.polimi.it>
2023-04-13 10:06:27 +0200

    d4

---
+++ fluids/d4/nicfd/p/coolprop.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 13:35:29 2022
+
+@author: yan
+
+compute normal shock relatiomnships for CoolProp EOS
+input: upstream P,T,M.
+
+"""
+
+from scipy.optimize import fsolve
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import CoolProp as CP
+from newIOpairs import TGfromZP,PGfromZT
+
+
+"""
+0. fluid property
+"""
+# print("--------------------Fluid info ------------------")
+fluidname = "D4"
+print("Fluid name:", fluidname)
+R = CP.CoolProp.PropsSI("gas_constant",fluidname)
+W = CP.CoolProp.PropsSI("molar_mass",fluidname)
+Rs = R/W
+print("spefific ags constant: J/Kg/K", Rs)
+Tc =  CP.CoolProp.PropsSI("Tcrit",fluidname)
+print("critical temperature[K]:", Tc)
+Pc =  CP.CoolProp.PropsSI("pcrit",fluidname)
+print("critical pressure[Pa]:", Pc)
+dc = CP.CoolProp.PropsSI('Dmass','P',Pc,'T',Tc,fluidname) 
+
+Z1 = np.arange(0.50, 0.98, 0.05).tolist()
+r_m = np.zeros(len(Z1)) # M2/M1
+r_p = np.zeros(len(Z1)) # P2/P1
+r_t = np.zeros(len(Z1)) # T2/T1
+r_d = np.zeros(len(Z1)) # D2/D1
+r_pt = np.zeros(len(Z1)) # PT2/PT1
+r_y = np.zeros(len(Z1)) # shock loss
+Diff = np.zeros(len(Z1)) # tolerance
+pfs= np.zeros(len(Z1)) # freestram pressure
+tfs= np.zeros(len(Z1)) # freestram temperature
+
+T1_list = np.arange(610, 650, 10).tolist()
+M1 = 1.5
+for k in range(0,len(T1_list),1):
+    for j in range(0,len(Z1),1):
+        """
+        1. pre-shock conditions
+        """
+        
+        print("--------------------pre-shock conditions ------------------")
+        T1 = T1_list[k] # total pressure
+        z1 = Z1[j]
+        P1, g1 = PGfromZT(z1,T1)
+        d1 = CP.CoolProp.PropsSI('Dmass','P',P1,'T',T1,fluidname) 
+        s = CP.CoolProp.PropsSI('Smass','P',P1,'T',T1,fluidname) 
+        h1 = CP.CoolProp.PropsSI('Hmass','P',P1,'T',T1,fluidname) 
+        c1 = CP.CoolProp.PropsSI('A','P',P1,'T',T1,fluidname) 
+        u1 = c1*M1
+        ht = h1 + 0.5*u1*u1
+        Pt1 = CP.CoolProp.PropsSI('P','Smass',s,'Hmass',ht,fluidname) 
+        """
+        2. input pre-shock conditions
+        """ 
+        
+        """
+        3. compute post-shock properites
+        """
+        # print("--------------------Post-shock states ------------------")
+        p2 = np.linspace(P1*1.1,P1*(1.9 + 0.05*j*5/2), 1000) # post-shock pressure 
+        p2 = pd.Series(p2)
+        u2 = np.zeros(p2.size) 
+        d2 = np.zeros(p2.size) 
+        diff = np.zeros(p2.size) + 1e3
+         
+        for i in p2.index:
+            if abs(p2[i]-Pc)<0.01*Pc:
+                p2[i] = 0.99*Pc
+            u2[i] = (P1+d1*u1*u1-p2[i])/d1/u1
+            if u2[i]>0:
+                d2[i] =  d1*u1/u2[i]
+                diff[i] = ht - 0.5*u2[i]*u2[i] - CP.CoolProp.PropsSI('Hmass','P',p2[i],'Dmass',d2[i],fluidname) 
+                if abs(diff[i])<1:
+                    print('diff is: ', diff[i])
+                    break
+        # print("min index:", np.argmin(abs(diff)))
+        P2 = p2[np.argmin(abs(diff))]
+        D2 = d2[np.argmin(abs(diff))]    
+        T2 = CP.CoolProp.PropsSI('T','P',P2,'Dmass',D2,fluidname) 
+        c2 = CP.CoolProp.PropsSI('A','P',P2,'Dmass',D2,fluidname) 
+        U2 = u2[np.argmin(abs(diff))]    
+        M2 = U2/c2
+        h2 = CP.CoolProp.PropsSI('Hmass','P',P2,'Dmass',D2,fluidname)
+        ht2 = h2 + 0.5*U2*U2
+        # print("ht2:", htotal2)
+        # print("(ht2-ht1)/ht1(%):", (ht2-ht)/ht*100)
+        Diff[j] = (ht2-ht)/ht*100
+        # print("M2:", M2)
+        # print("P2/P1:", P2/P1)
+        # print("T2/T1:", T2/T1)
+        # print("D2/D1:", D2/d1)
+        s2 = CP.CoolProp.PropsSI('Smass','P',P2,'Dmass',D2,fluidname) 
+        Pt2 = CP.CoolProp.PropsSI('P','Smass',s2,'Hmass',ht2,fluidname) 
+        r_m[j] = M2/M1
+        r_p[j] = P2/P1
+        r_t[j] = T2/T1
+        r_d[j] = D2/d1
+        r_pt[j] = Pt2/Pt1
+        r_y[j] = (Pt1-Pt2)/(Pt1-P1)*100
+        pfs[j] = P1
+        tfs[j] = T1
+        # Tt2 = CP.CoolProp.PropsSI('T','Smass',s2,'Hmass',ht2,fluidname) 
+        # Y = (pt-Pt2)/(pt-P1)*100
+    
+    """
+    4. write into csv file
+    """   
+    data = 'data' + str(k) + '.csv'
+    pd.DataFrame(Z1).to_csv(data, index_label = "Index", header  = ['Z1']) 
+    result = pd.read_csv(data, ",")
+    # append new columns
+    D =pd.DataFrame({'P2/P1': r_p, 'T2/T1': r_t, 'D2/D1': r_d,'M2/M1': r_m, 'Pt2/Pt1': r_pt,'Y': r_y, 
+                     'diff': Diff, 'P1': pfs, 'T1': tfs, })
+    newData = pd.concat([result, D], join = 'outer', axis = 1)
+    # save newData in csv file
+    # newData.to_csv("m4sh.csv")
+    newData.to_csv(data)
---
+++ fluids/d4/nicfd/t/plot.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 14:59:53 2023
+
+@author: yan
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+"""
+read csv file
+"""
+T1_list = np.arange(610, 650, 10).tolist()
+nc = len(T1_list)
+colors = plt.cm.tab20(np.linspace(0, 1, nc))
+
+"""
+plot 
+"""
+nn = 10 # number of columes in data.csv
+fig1 = plt.figure( dpi=300)
+lw = 2
+axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+pmax = np.zeros(nn)
+pmin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,3],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        pmax[i] = max(pmax[i],z.iloc[i,3])
+        pmin[i] = min(pmin[i],z.iloc[i,3])
+    
+ax2.plot(z.iloc[:,2] , (pmax-pmin)/pmax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+# ax2.set_ylim([0, 3])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# axes.set_xlim([0.5, 1.0])
+# axes.set_ylim([1.6, 2.3])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$P_2/P_1$',fontsize=12) 
+axes.legend(loc=8 , prop={'size': 10}) # 
+axes.set_title('$P_2/P_1$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig1.savefig("files/nicfd_t_d4_rp.eps")
+
+fig2 = plt.figure( dpi=300)
+lw = 2
+axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+tmax = np.zeros(nn)
+tmin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,4],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        tmax[i] = max(tmax[i],z.iloc[i,4])
+        tmin[i] = min(tmin[i],z.iloc[i,4])
+    
+ax2.plot(z.iloc[:,2] , (tmax-tmin)/tmax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+# ax2.set_ylim([0, 3])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlim([0.5, 1.0])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$T_2/T_1$',fontsize=12) 
+axes.legend(loc=0 , prop={'size': 10}) # 
+axes.set_title('$T_2/T_1$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig2.savefig("files/nicfd_t_d4_rt.eps")
+
+fig3 = plt.figure( dpi=300)
+lw = 2
+axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+dmax = np.zeros(nn)
+dmin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,5],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        dmax[i] = max(dmax[i],z.iloc[i,5])
+        dmin[i] = min(dmin[i],z.iloc[i,5])
+    
+ax2.plot(z.iloc[:,2] , (dmax-dmin)/dmax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+# ax2.set_ylim([0, 3])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlim([0.5, 1.0])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$\\rho_2/\\rho_1$',fontsize=12) 
+axes.legend(loc=8 , prop={'size': 10}) # 
+axes.set_title('$\\rho_2/\\rho_1$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig3.savefig("files/nicfd_t_d4_rd.eps")
+
+fig4 = plt.figure( dpi=300)
+lw = 2
+axes = fig4.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+mmax = np.zeros(nn)
+mmin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,6],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        mmax[i] = max(mmax[i],z.iloc[i,6])
+        mmin[i] = min(mmin[i],z.iloc[i,6])
+    
+ax2.plot(z.iloc[:,2] , (mmax-mmin)/mmax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+# ax2.set_ylim([0, 3])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlim([0.5, 1.0])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$M_2/M_1$',fontsize=12) 
+axes.legend(loc=5 , prop={'size': 10}) # 
+axes.set_title('$M_2/M_1$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig4.savefig("files/nicfd_t_d4_rm.eps")
+
+fig5 = plt.figure( dpi=300)
+lw = 2
+axes = fig5.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+ptmax = np.zeros(nn)
+ptmin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,7],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        ptmax[i] = max(ptmax[i],z.iloc[i,7])
+        ptmin[i] = min(ptmin[i],z.iloc[i,7])
+    
+ax2.plot(z.iloc[:,2] , (ptmax-ptmin)/ptmax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+ax2.set_ylim([0, 5])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlim([0.5, 1.0])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$P_{t2}/P_{t1}$',fontsize=12) 
+axes.legend(loc=0 , prop={'size': 10}) # 
+axes.set_title('$P_{t2}/P_{t1}$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig5.savefig("files/nicfd_t_d4_rpt.eps")
+
+fig6 = plt.figure( dpi=300)
+lw = 2
+axes = fig6.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+ax2 = axes.twinx()
+ymax = np.zeros(nn)
+ymin = np.zeros(nn)+100
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,8],'o', color=colors[k],  lw = lw, label = lb)
+    for i in range(nn):
+        ymax[i] = max(ymax[i],z.iloc[i,8])
+        ymin[i] = min(ymin[i],z.iloc[i,8])
+    
+ax2.plot(z.iloc[:,2] , (ymax-ymin)/ymax*100 , 'k*', lw=lw)
+ax2.set_ylabel('diff(%)',fontsize=12) 
+# ax2.set_ylim([0, 3])
+ax2.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.set_xlim([0.5, 1.0])
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('Y(%)',fontsize=12) 
+axes.legend(loc=0 , prop={'size': 10}) # 
+axes.set_title('Y(%) vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+axes.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+fig6.savefig("files/nicfd_t_d4_ry.eps")
+
+
+
+
+
+fig10 = plt.figure( dpi=300)
+lw = 2
+axes = fig10.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+for k in range(0,len(T1_list),1):
+    data = 'data' + str(k) + '.csv'
+    z = pd.read_csv(data, ",", skiprows=0)
+    lb = 'T1=' + str(T1_list[k]) + '(K)'
+    axes.plot(z.iloc[:,2],z.iloc[:,-3],'o', color=colors[k],  lw = lw, label = lb)
+axes.set_xlabel('$Z_1$',fontsize=12)
+axes.set_ylabel('$(h_{t2}-h_{t1})/h_{t2}(\%)$',fontsize=12) 
+axes.legend(loc=0 , prop={'size': 10}) # 
+axes.set_title('$(h_{t2}-h_{t1})/h_{t1}(\%)$ vs $Z_1$',fontsize=14)
+axes.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+# axes.set_ylim([0, 0.1])
+# fig1.savefig("files/nicfd_mm_rh.eps")
+
+
